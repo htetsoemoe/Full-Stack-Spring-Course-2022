eda76b7255b44d8f2765f51df18406fcfa27adcb
htetsoemoe <soemoehtet.mdy@gmail.com>
2023-02-03 15:05:58 +0630

    thymeleaf view with spring boots

---
+++ thymeleaf-getting-start-spring-boot/src/main/java/com/jdc/demo/ThymeleafGettingStartSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.jdc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafGettingStartSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafGettingStartSpringBootApplication.class, args);
+	}
+
+}
